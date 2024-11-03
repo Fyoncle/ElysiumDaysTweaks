ad502edb863d9bfc0cf4f418bdfce9b813691f34
VipCoder8 <114797944+VipCoder8@users.noreply.github.com>
2024-11-03 13:19:12 +0300

    Organized lining.

--- src/main/java/net/fyoncle/elysiumdaystweaks/utility/constants/Textures.java
+++ src/main/java/net/fyoncle/elysiumdaystweaks/utility/constants/Textures.java
@@ -4,45 +4,59 @@ import net.minecraft.util.Identifier;
 
 public class Textures {
  // Health Bar Textures
-    public static Identifier UNFOCUSED_OFF_HEALTHBAR_TEXTURE = new Identifier("elysium-days-tweaks",
+    public static Identifier UNFOCUSED_OFF_HEALTHBAR_TEXTURE = new Identifier(
+            "elysium-days-tweaks",
             "buttons/neat_disable_button_unfocused.png");
-    public static Identifier UNFOCUSED_ON_HEALTHBAR_TEXTURE = new Identifier("elysium-days-tweaks",
+    public static Identifier UNFOCUSED_ON_HEALTHBAR_TEXTURE = new Identifier(
+            "elysium-days-tweaks",
             "buttons/neat_button_unfocused.png");
 
-    public static Identifier FOCUSED_ON_HEALTHBAR_TEXTURE = new Identifier("elysium-days-tweaks",
+    public static Identifier FOCUSED_ON_HEALTHBAR_TEXTURE = new Identifier(
+            "elysium-days-tweaks",
             "buttons/neat_button_focused.png");
-    public static Identifier FOCUSED_OFF_HEALTHBAR_TEXTURE = new Identifier("elysium-days-tweaks",
+    public static Identifier FOCUSED_OFF_HEALTHBAR_TEXTURE = new Identifier(
+            "elysium-days-tweaks",
             "buttons/neat_disable_button_focused.png");
 
     // Discord Button Textures
-    public static Identifier DISCORD_BUTTON_UNFOCUSED_TEXTURE = new Identifier("elysium-days-tweaks",
+    public static Identifier DISCORD_BUTTON_UNFOCUSED_TEXTURE = new Identifier(
+            "elysium-days-tweaks",
             "buttons/discord_button_unfocused.png");
-    public static Identifier DISCORD_BUTTON_FOCUSED_TEXTURE = new Identifier("elysium-days-tweaks",
+    public static Identifier DISCORD_BUTTON_FOCUSED_TEXTURE = new Identifier(
+            "elysium-days-tweaks",
             "buttons/discord_button_focused.png");
 
     // Green Button Texture
-    public static Identifier GREEN_BUTTON_FOCUSED_TEXTURE = new Identifier("elysium-days-tweaks",
+    public static Identifier GREEN_BUTTON_FOCUSED_TEXTURE = new Identifier(
+            "elysium-days-tweaks",
             "buttons/green_button_focused.png");
-    public static Identifier GREEN_BUTTON_UNFOCUSED_TEXTURE = new Identifier("elysium-days-tweaks",
+    public static Identifier GREEN_BUTTON_UNFOCUSED_TEXTURE = new Identifier(
+            "elysium-days-tweaks",
             "buttons/green_button_unfocused.png");
 
  // Ram Warning Screen Buttons Texture
 
     // Ignore Button
-    public static Identifier IGNORE_BUTTON_FOCUSED = new Identifier("elysium-days-tweaks",
+    public static Identifier IGNORE_BUTTON_FOCUSED = new Identifier(
+            "elysium-days-tweaks",
             "buttons/default_button_focused.png");
-    public static Identifier IGNORE_BUTTON_UNFOCUSED = new Identifier("elysium-days-tweaks",
+    public static Identifier IGNORE_BUTTON_UNFOCUSED = new Identifier(
+            "elysium-days-tweaks",
             "buttons/default_button_unfocused.png");
 
     // Guide Button
-    public static Identifier GUIDE_BUTTON_FOCUSED = new Identifier("elysium-days-tweaks",
+    public static Identifier GUIDE_BUTTON_FOCUSED = new Identifier(
+            "elysium-days-tweaks",
             "buttons/guide_button_focused.png");
-    public static Identifier GUIDE_BUTTON_UNFOCUSED = new Identifier("elysium-days-tweaks",
+    public static Identifier GUIDE_BUTTON_UNFOCUSED = new Identifier(
+            "elysium-days-tweaks",
             "buttons/guide_button_unfocused.png");
 
     // Dont Show Button
-    public static Identifier RED_BUTTON_FOCUSED = new Identifier("elysium-days-tweaks",
+    public static Identifier RED_BUTTON_FOCUSED = new Identifier(
+            "elysium-days-tweaks",
             "buttons/red_button_focused.png");
-    public static Identifier RED_BUTTON_UNFOCUSED = new Identifier("elysium-days-tweaks",
+    public static Identifier RED_BUTTON_UNFOCUSED = new Identifier(
+            "elysium-days-tweaks",
             "buttons/red_button_unfocused.png");
 }
