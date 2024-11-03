16a5a4ff0123722f8ee7d6f9ff22d1fd705de643
VipCoder8 <114797944+VipCoder8@users.noreply.github.com>
2024-11-03 21:19:54 +0300

    Added Resource Pack lock.

---
+++ src/main/java/net/fyoncle/elysiumdaystweaks/mixin/ResourcePackLock.java
@@ -0,0 +1,20 @@
+package net.fyoncle.elysiumdaystweaks.mixin;
+
+import net.minecraft.resource.ResourcePackProfile;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ResourcePackProfile.class)
+public abstract class ResourcePackLock {
+    @Shadow public abstract String getName();
+
+    @Inject(at = @At("RETURN"), method = "isPinned", cancellable = true)
+    public void isPinned(CallbackInfoReturnable<Boolean> cir) {
+        if(this.getName().equals("elysium-days-tweaks:elysiumdaystweaks")) {
+            cir.setReturnValue(true);
+        }
+    }
+}
