4e803def5d7c20470d3f64aeee6f29cc6f7dc4c6
VipCoder8 <114797944+VipCoder8@users.noreply.github.com>
2025-01-24 19:29:55 +0300

    Fixed resource pack lock issues.

--- src/main/java/net/fyoncle/elysiumdaystweaks/mixin/ResourcePackLock.java
+++ src/main/java/net/fyoncle/elysiumdaystweaks/mixin/ResourcePackLock.java
@@ -1,6 +1,7 @@
 package net.fyoncle.elysiumdaystweaks.mixin;
 
 import net.minecraft.resource.ResourcePackProfile;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -9,19 +10,28 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ResourcePackProfile.class)
 public abstract class ResourcePackLock {
+    private final String ED_RESOURCE_PACK = "elysium-days-tweaks:elysiumdaystweaks";
+    private final String[] EXCLUDE_FROM_UNPIN_RPS = {
+            ED_RESOURCE_PACK,
+            "__moremcmeta-internal__"
+    };
     @Shadow public abstract String getName();
 
     @Inject(at = @At("RETURN"), method = "getInitialPosition", cancellable = true)
     public void getInitialPosition(CallbackInfoReturnable<ResourcePackProfile.InsertionPosition> cir) {
-        if(this.getName().equals("elysium-days-tweaks:elysiumdaystweaks")) {
-            cir.setReturnValue(ResourcePackProfile.InsertionPosition.TOP);
+        for (String excludeFromUnpinRp : EXCLUDE_FROM_UNPIN_RPS) {
+            if (this.getName().equals(excludeFromUnpinRp)) {
+                cir.setReturnValue(ResourcePackProfile.InsertionPosition.TOP);
+            }
         }
     }
 
     @Inject(at = @At("RETURN"), method = "isPinned", cancellable = true)
     public void isPinned(CallbackInfoReturnable<Boolean> cir) {
-        if(this.getName().equals("elysium-days-tweaks:elysiumdaystweaks")) {
-            cir.setReturnValue(true);
+        for(int i = 0; i < EXCLUDE_FROM_UNPIN_RPS.length; i++) {
+            if(this.getName().equals(EXCLUDE_FROM_UNPIN_RPS[i])) {
+                cir.setReturnValue(true);
+            }
         }
     }
 }
