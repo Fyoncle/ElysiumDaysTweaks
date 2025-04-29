11e17f24e9f2637fba3b9b7154b981e96691a937
Fyoncle <muratcan_mete050@hotmail.com>
2025-04-29 20:34:59 +0300

    Move Textures to their proper places

--- src/main/java/net/fyoncle/elysiumdaystweaks/utility/constants/Textures.java
+++ src/main/java/net/fyoncle/elysiumdaystweaks/utility/constants/Textures.java
@@ -24,54 +24,54 @@ public class Textures {
     // OFF
     public static Identifier UNFOCUSED_OFF_HEALTHBAR_TEXTURE = new Identifier(
             "elysium-days-tweaks",
-            "buttons/neat_disabled_button_unfocused.png");
+            "textures/gui/neat_disabled_button_unfocused.png");
     public static Identifier FOCUSED_OFF_HEALTHBAR_TEXTURE = new Identifier(
             "elysium-days-tweaks",
-            "buttons/neat_disabled_button_focused.png");
+            "textures/gui/neat_disabled_button_focused.png");
     // ON
     public static Identifier UNFOCUSED_ON_HEALTHBAR_TEXTURE = new Identifier(
             "elysium-days-tweaks",
-            "buttons/neat_enabled_button_unfocused.png");
+            "textures/gui/neat_enabled_button_unfocused.png");
     public static Identifier FOCUSED_ON_HEALTHBAR_TEXTURE = new Identifier(
             "elysium-days-tweaks",
-            "buttons/neat_enabled_button_focused.png");
+            "textures/gui/neat_enabled_button_focused.png");
     // - Discord Button Textures -
     public static Identifier DISCORD_BUTTON_UNFOCUSED_TEXTURE = new Identifier(
             "elysium-days-tweaks",
-            "buttons/discord_button_unfocused.png");
+            "textures/gui/discord_button_unfocused.png");
     public static Identifier DISCORD_BUTTON_FOCUSED_TEXTURE = new Identifier(
             "elysium-days-tweaks",
-            "buttons/discord_button_focused.png");
+            "textures/gui/discord_button_focused.png");
     // - Ram Warning Screen Textures -
     // Guide Button
     public static Identifier GUIDE_BUTTON_UNFOCUSED = new Identifier(
             "elysium-days-tweaks",
-            "buttons/guide_button_unfocused.png");
+            "textures/gui/guide_button_unfocused.png");
     public static Identifier GUIDE_BUTTON_FOCUSED = new Identifier(
             "elysium-days-tweaks",
-            "buttons/guide_button_focused.png");
+            "textures/gui/guide_button_focused.png");
     // - Other -
     // Default Button
     public static Identifier DEFAULT_BUTTON_UNFOCUSED = new Identifier(
             "elysium-days-tweaks",
-            "buttons/default_button_unfocused.png");
+            "textures/gui/default_button_unfocused.png");
     public static Identifier DEFAULT_BUTTON_FOCUSED = new Identifier(
             "elysium-days-tweaks",
-            "buttons/default_button_focused.png");
+            "textures/gui/default_button_focused.png");
 
     // Red Button
     public static Identifier RED_BUTTON_UNFOCUSED = new Identifier(
             "elysium-days-tweaks",
-            "buttons/red_button_unfocused.png");
+            "textures/gui/red_button_unfocused.png");
     public static Identifier RED_BUTTON_FOCUSED = new Identifier(
             "elysium-days-tweaks",
-            "buttons/red_button_focused.png");
+            "textures/gui/red_button_focused.png");
 
     // Green Button Texture
     public static Identifier GREEN_BUTTON_UNFOCUSED_TEXTURE = new Identifier(
             "elysium-days-tweaks",
-            "buttons/green_button_unfocused.png");
+            "textures/gui/green_button_unfocused.png");
     public static Identifier GREEN_BUTTON_FOCUSED_TEXTURE = new Identifier(
             "elysium-days-tweaks",
-            "buttons/green_button_focused.png");
+            "textures/gui/green_button_focused.png");
 }
