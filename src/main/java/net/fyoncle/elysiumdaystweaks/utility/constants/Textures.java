1a6682965e22a8d51129910ffd05d479085cecbb
Fyoncle <muratcan_mete050@hotmail.com>
2025-05-03 04:09:46 +0300

    Clean Up

--- src/main/java/net/fyoncle/elysiumdaystweaks/utility/constants/Textures.java
+++ src/main/java/net/fyoncle/elysiumdaystweaks/utility/constants/Textures.java
@@ -4,15 +4,17 @@ import net.minecraft.util.Identifier;
 
 public class Textures {
     // - Menu Logos -
-    // Default Logo
+      // Default Logo
     public static final Identifier ED_DEFAULT_LOGO = new Identifier("elysium-days-tweaks",
             "logos/eddefaultlogo.png");
-    // Halloween Logo
+      // Halloween Logo
     public static final Identifier ED_HALLOWEEN_LOGO = new Identifier("elysium-days-tweaks",
             "logos/edhalloweenlogo.png");
-    // Christmas Logo
+      // Christmas Logo
     public static final Identifier ED_CHRISTMAS_LOGO = new Identifier("elysium-days-tweaks",
             "logos/edchristmaslogo.png");
+
+
     // - Menu Panoramas -
     public static final Identifier ED_DEFAULT_PANORAMA = new Identifier("elysium-days-tweaks",
             "panoramas/default_panorama.png");
@@ -20,15 +22,17 @@ public class Textures {
             "panoramas/halloween_panorama.png");
     public static final Identifier ED_CHRISTMAS_PANORAMA = new Identifier("elysium-days-tweaks",
             "panoramas/christmas_panorama.png");
+
+
     // - Health Bar Textures -
-    // OFF
+      // OFF
     public static Identifier UNFOCUSED_OFF_HEALTHBAR_TEXTURE = new Identifier(
             "elysium-days-tweaks",
             "textures/gui/neat_disabled_button_unfocused.png");
     public static Identifier FOCUSED_OFF_HEALTHBAR_TEXTURE = new Identifier(
             "elysium-days-tweaks",
             "textures/gui/neat_disabled_button_focused.png");
-    // ON
+      // ON
     public static Identifier UNFOCUSED_ON_HEALTHBAR_TEXTURE = new Identifier(
             "elysium-days-tweaks",
             "textures/gui/neat_enabled_button_unfocused.png");
@@ -42,32 +46,34 @@ public class Textures {
     public static Identifier DISCORD_BUTTON_FOCUSED_TEXTURE = new Identifier(
             "elysium-days-tweaks",
             "textures/gui/discord_button_focused.png");
+
+
     // - Ram Warning Screen Textures -
-    // Guide Button
+      // Guide Button
     public static Identifier GUIDE_BUTTON_UNFOCUSED = new Identifier(
             "elysium-days-tweaks",
             "textures/gui/guide_button_unfocused.png");
     public static Identifier GUIDE_BUTTON_FOCUSED = new Identifier(
             "elysium-days-tweaks",
             "textures/gui/guide_button_focused.png");
+
+
     // - Other -
-    // Default Button
+      // Default Button
     public static Identifier DEFAULT_BUTTON_UNFOCUSED = new Identifier(
             "elysium-days-tweaks",
             "textures/gui/default_button_unfocused.png");
     public static Identifier DEFAULT_BUTTON_FOCUSED = new Identifier(
             "elysium-days-tweaks",
             "textures/gui/default_button_focused.png");
-
-    // Red Button
+      // Red Button
     public static Identifier RED_BUTTON_UNFOCUSED = new Identifier(
             "elysium-days-tweaks",
             "textures/gui/red_button_unfocused.png");
     public static Identifier RED_BUTTON_FOCUSED = new Identifier(
             "elysium-days-tweaks",
             "textures/gui/red_button_focused.png");
-
-    // Green Button Texture
+      // Green Button Texture
     public static Identifier GREEN_BUTTON_UNFOCUSED_TEXTURE = new Identifier(
             "elysium-days-tweaks",
             "textures/gui/green_button_unfocused.png");
