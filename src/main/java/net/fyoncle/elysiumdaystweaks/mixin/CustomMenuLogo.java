e76ca32c261a7a9c251e4453a075877284a6ac65
Fyoncle <63975256+Fyoncle@users.noreply.github.com>
2025-06-04 03:52:23 +0300

    Remove Edition Logo through @WrapOperation instead of relaying on a resource pack

--- src/main/java/net/fyoncle/elysiumdaystweaks/mixin/CustomMenuLogo.java
+++ src/main/java/net/fyoncle/elysiumdaystweaks/mixin/CustomMenuLogo.java
@@ -32,4 +32,14 @@ public class CustomMenuLogo {
             original.call(instance, resourceLocation, i, j, 0f, 0f, 256, 70, 256, 70);
         }
     }
+
+    @WrapOperation(
+            method = "draw(Lnet/minecraft/client/gui/DrawContext;IFI)V",
+            at = @At(value = "INVOKE", ordinal = 1, target = "Lnet/minecraft/client/gui/DrawContext;drawTexture(Lnet/minecraft/util/Identifier;IIFFIIII)V")
+    )
+    public void elysiumDaysTweaks$removeEditionLogo(DrawContext instance, Identifier resourceLocation,
+                                                    int i, int j, float f,
+                                                    float g, int k,
+                                                    int l, int m, int n, Operation<Void> original) {
+    }
 }
